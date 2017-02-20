dfd261f144c0ae914f4d6fe6c259b058394076c2
Frank <ifranagu@hotmail.com>
2017-02-20 19:40:18 +0100

    arreglado error en buscadorlocal.sql

--- README.md
+++ README.md
@@ -7,13 +7,13 @@
 # Proyecto fin de curso DAW - Symfony marzo 2017
 ## By Paulino Francisco Angulo Grancha
 
-### buscadorlocal
+### buscadorlocal (Catarroja -> Valencia -> Spain)
 
 
 Pasos para poner a nivel local la aplicación en marcha:
 
 Se debe tener instalado servidor apache, mysql y composer.
-Yo realicé este proyecto en wuindows 8.1 – Utilicé xampp, composer y symfony 2.8
+Yo realicé este proyecto en windows 8.1 – Utilicé xampp, composer y symfony 2.8
 
 El paso “0” es poner en marcha el servidor apache y el mysql
 
@@ -21,7 +21,7 @@ El paso “0” es poner en marcha el servidor apache y el mysql
 2. Situarse dentro de la carpeta del proyecto y ejecutar “composer install” o “composer update”
 3. Crear en mysql una base de datos con cotejamiento "UTF8-spanish"
 4. En el fichero security.yml poner los parámetros para su base de datos.
-5. Ejecutar: php app/console doctrine:schema
+5. Ejecutar: php app/console doctrine:schema:create
 6. Ir a phpMyAdmin y dentro de la base de datos que haya creado en el paso nº 3, importar el archivo “buscadorlocal.sql” que adjunto con el proyecto.
 7. Ejecutar: php app/console server:run
 
@@ -32,6 +32,8 @@ Si todo ha ido bien, ya se puede ir al navegador y poner en la URL:
 
 http://localhost:8000
 
+
 Si no funciona:
+
 http://127.0.0.1:8000
 
--- buscadorlocal.sql
+++ buscadorlocal.sql
@@ -1,40 +1,65 @@
+
 -- phpMyAdmin SQL Dump
 -- version 4.6.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
 -- Tiempo de generación: 07-02-2017 a las 15:24:54
--- Versión del servidor: 10.1.9-MariaDB
+-- Servidor: localhost
 -- Versión de PHP: 5.6.15
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `buscadorlocal`
---
 
 -- --------------------------------------------------------
 --
--- Volcado de datos para la tabla `anuncio`
+-- Volcado de datos para la tabla `fos_user`
 --
 
-INSERT INTO `anuncio` (`id`, `tipoanuncio_id`, `persona_id`, `fecha`, `titulo`, `texto`) VALUES
-(1, 1, NULL, '2017-02-01', 'Presentaciones de comisiones Falleras', 'En estos días se abre el plazo de inscripción de todos los falleros y falleras que quieran participar en este evento. Acudir al Ayuntamiento en horario de oficina para realizar el trámite. Es recomendable no dejarlo para último momento.'),
-(2, 4, NULL, '2017-03-01', 'Calles cortadas por Fallas', 'ATENCIÓN, Las calles del centro de la población estará cortadas de forma gradual por motivo de la instalación de los monumentos falleros, así como las carpas de algunos casales falleros. Desde la Policía Local les deseamos que disfruten de las fiestas. Seguiremos informando.'),
-(4, 5, NULL, '05-01-2017', 'Cabalgata de sus Majestades Los Reyes Magos.', 'Con motivo de la Cabalgata de sus Majestades Los Reyes Magos, se aconseja asistir a la misma en transporte público o caminando, a fin de no colapsar las calles. Esperemos que os dejen lo que les habéis pedido, y que os porteis bien.'),
-(5, 3, NULL, '06-01-2017', 'Partido de futbol en el polideportivo', 'El próximo domingo día 8 de enero de 2017, se celebrará un partido de futbol entre los colegios San José y Sant Pere. La entrada será de tan solo un Euro y lo recaudado será donado íntegramente a Cáritas. Esperamos su asistencia.');
+INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
+(1, 'admin', 'admin', 'segundo@kiupa.com', 'segundo@kiupa.com', 1, 'iq1wk3hdko0kk8sgwgcwckoow0cosgo', '$2y$13$iq1wk3hdko0kk8sgwgcwceWqlvl.Q12bdCEBHb1JVGnxbhzCrqcR6', '2017-02-01 19:17:32', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL),
+(2, 'user', 'user', 'segundo@kiupa.es', 'segundo@kiupa.es', 1, '985rnqowqz8cc04o4g080cwokg0gswg', '$2y$13$985rnqowqz8cc04o4g080Oqa7if6QOYyrT69Fh6HihO1jXRh3.0TK', '2017-01-30 23:54:50', 0, 0, NULL, NULL, NULL, 'a:0:{}', 0, NULL);
+
 
 -- --------------------------------------------------------
 --
--- Volcado de datos para la tabla `buscar`
+-- Volcado de datos para la tabla `tipocomercio`
 --
+
+INSERT INTO `tipocomercio` (`id`, `nombre`) VALUES
+(8, 'Abogados'),
+(1, 'Bares'),
+(6, 'Floristerías'),
+(7, 'Fontanería'),
+(5, 'Modas'),
+(3, 'Panaderías'),
+(2, 'Pinturas'),
+(4, 'Zapaterías');
+
+
+-- --------------------------------------------------------
+--
+-- Volcado de datos para la tabla `tipoanuncio`
+--
+
+INSERT INTO `tipoanuncio` (`id`, `nombre`) VALUES
+(1, 'Ayuntamiento'),
+(3, 'Deportes'),
+(2, 'Fallas'),
+(5, 'Navidad'),
+(4, 'Policía Local'),
+(6, 'Sin Clasificar');
+
+
+-- --------------------------------------------------------
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `telefono`, `email`, `genero`, `catarroja`) VALUES
+(1, 'Antonio', 'Galvez', 41, 961272584, 'galvez@gmail.com', 'm', 1),
+(2, 'Manuel', 'Molinos', 53, 961261264, 'manuel.molinos@gmail.com', 'm', 1),
+(3, 'No', 'Asignado', 0, 0, 'null@null.null', 'm', 0),
+(4, 'Maruja', 'Pérez', 60, 961265837, 'panaderiaqmaru@maru.com', 'f', 0);
+
 
 INSERT INTO `buscar` (`id`, `tipoanuncio_id`, `criterio`, `fecha`) VALUES
 (7, 2, 'Pinturas', '04-01-2017'),
@@ -58,6 +83,14 @@ INSERT INTO `buscar` (`id`, `tipoanuncio_id`, `criterio`, `fecha`) VALUES
 (25, 3, 'Panaderías', '30-01-2017'),
 (26, 3, 'Panaderías', '30-01-2017');
 
+
+INSERT INTO `anuncio` (`id`, `tipoanuncio_id`, `persona_id`, `fecha`, `titulo`, `texto`) VALUES
+(1, 1, NULL, '2017-02-01', 'Presentaciones de comisiones Falleras', 'En estos días se abre el plazo de inscripción de todos los falleros y falleras que quieran participar en este evento. Acudir al Ayuntamiento en horario de oficina para realizar el trámite. Es recomendable no dejarlo para último momento.'),
+(2, 4, NULL, '2017-03-01', 'Calles cortadas por Fallas', 'ATENCIÓN, Las calles del centro de la población estará cortadas de forma gradual por motivo de la instalación de los monumentos falleros, así como las carpas de algunos casales falleros. Desde la Policía Local les deseamos que disfruten de las fiestas. Seguiremos informando.'),
+(4, 5, NULL, '05-01-2017', 'Cabalgata de sus Majestades Los Reyes Magos.', 'Con motivo de la Cabalgata de sus Majestades Los Reyes Magos, se aconseja asistir a la misma en transporte público o caminando, a fin de no colapsar las calles. Esperemos que os dejen lo que les habéis pedido, y que os porteis bien.'),
+(5, 3, NULL, '06-01-2017', 'Partido de futbol en el polideportivo', 'El próximo domingo día 8 de enero de 2017, se celebrará un partido de futbol entre los colegios San José y Sant Pere. La entrada será de tan solo un Euro y lo recaudado será donado íntegramente a Cáritas. Esperamos su asistencia.');
+
+
 -- --------------------------------------------------------
 --
 -- Volcado de datos para la tabla `comercio`
@@ -74,176 +107,3 @@ INSERT INTO `comercio` (`id`, `tipocomercio_id`, `persona_id`, `posicion`, `nomb
 (9, 8, 1, 15, 'Prueba_15', 'C/ Prueba_15', 363346860, 'http://www.kiupa.com', '363346860', '363346860', 'qwerty@qwerty'),
 (10, 3, 2, 1, 'Horno Bolleria Manuel Molinos', 'Carrer Constitució, 9, 46470 Catarroja, València', 961261264, 'http://www.hornomanuelmolinos.es', '961261264', '961261264', 'manuel.molinos@gmail.com'),
 (11, 3, 4, 2, 'Horno Pasteleria Maru', 'Pl. Mestre Iturbi, 9. 46470. CATARROJA, VALENCIA', 961265837, 'http://www.kiupa.com', '961265837', '961265837', 'panaderiaqmaru@maru.com');
-
--- --------------------------------------------------------
---
--- Volcado de datos para la tabla `fos_user`
---
-
-INSERT INTO `fos_user` (`id`, `username`, `username_canonical`, `email`, `email_canonical`, `enabled`, `salt`, `password`, `last_login`, `locked`, `expired`, `expires_at`, `confirmation_token`, `password_requested_at`, `roles`, `credentials_expired`, `credentials_expire_at`) VALUES
-(1, 'admin', 'admin', 'segundo@kiupa.com', 'segundo@kiupa.com', 1, 'iq1wk3hdko0kk8sgwgcwckoow0cosgo', '$2y$13$iq1wk3hdko0kk8sgwgcwceWqlvl.Q12bdCEBHb1JVGnxbhzCrqcR6', '2017-02-01 19:17:32', 0, 0, NULL, NULL, NULL, 'a:1:{i:0;s:10:"ROLE_ADMIN";}', 0, NULL),
-(2, 'user', 'user', 'segundo@kiupa.es', 'segundo@kiupa.es', 1, '985rnqowqz8cc04o4g080cwokg0gswg', '$2y$13$985rnqowqz8cc04o4g080Oqa7if6QOYyrT69Fh6HihO1jXRh3.0TK', '2017-01-30 23:54:50', 0, 0, NULL, NULL, NULL, 'a:0:{}', 0, NULL);
-
--- --------------------------------------------------------
---
--- Volcado de datos para la tabla `persona`
---
-
-INSERT INTO `persona` (`id`, `nombre`, `apellido`, `edad`, `telefono`, `email`, `genero`, `catarroja`) VALUES
-(1, 'Antonio', 'Galvez', 41, 961272584, 'galvez@gmail.com', 'm', 1),
-(2, 'Manuel', 'Molinos', 53, 961261264, 'manuel.molinos@gmail.com', 'm', 1),
-(3, 'No', 'Asignado', 0, 0, 'null@null.null', 'm', 0),
-(4, 'Maruja', 'Pérez', 60, 961265837, 'panaderiaqmaru@maru.com', 'f', 0);
-
--- --------------------------------------------------------
---
--- Volcado de datos para la tabla `tipoanuncio`
---
-
-INSERT INTO `tipoanuncio` (`id`, `nombre`) VALUES
-(1, 'Ayuntamiento'),
-(3, 'Deportes'),
-(2, 'Fallas'),
-(5, 'Navidad'),
-(4, 'Policía Local'),
-(6, 'Sin Clasificar');
-
--- --------------------------------------------------------
---
--- Volcado de datos para la tabla `tipocomercio`
---
-
-INSERT INTO `tipocomercio` (`id`, `nombre`) VALUES
-(8, 'Abogados'),
-(1, 'Bares'),
-(6, 'Floristerías'),
-(7, 'Fontanería'),
-(5, 'Modas'),
-(3, 'Panaderías'),
-(2, 'Pinturas'),
-(4, 'Zapaterías');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `anuncio`
---
-ALTER TABLE `anuncio`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_4B3BC0D439C278AE` (`tipoanuncio_id`),
-  ADD KEY `IDX_4B3BC0D4F5F88DB9` (`persona_id`);
-
---
--- Indices de la tabla `buscar`
---
-ALTER TABLE `buscar`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `IDX_343A50B039C278AE` (`tipoanuncio_id`);
-
---
--- Indices de la tabla `comercio`
---
-ALTER TABLE `comercio`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_419511CDC1E70A7F` (`telefono`),
-  ADD KEY `IDX_419511CDDE4776E9` (`tipocomercio_id`),
-  ADD KEY `IDX_419511CDF5F88DB9` (`persona_id`);
-
---
--- Indices de la tabla `fos_user`
---
-ALTER TABLE `fos_user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_957A647992FC23A8` (`username_canonical`),
-  ADD UNIQUE KEY `UNIQ_957A6479A0D96FBF` (`email_canonical`);
-
---
--- Indices de la tabla `persona`
---
-ALTER TABLE `persona`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_51E5B69BC1E70A7F` (`telefono`),
-  ADD UNIQUE KEY `UNIQ_51E5B69BE7927C74` (`email`);
-
---
--- Indices de la tabla `tipoanuncio`
---
-ALTER TABLE `tipoanuncio`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_27B1D14F3A909126` (`nombre`);
-
---
--- Indices de la tabla `tipocomercio`
---
-ALTER TABLE `tipocomercio`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `UNIQ_2624D1103A909126` (`nombre`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `anuncio`
---
-ALTER TABLE `anuncio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT de la tabla `buscar`
---
-ALTER TABLE `buscar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
---
--- AUTO_INCREMENT de la tabla `comercio`
---
-ALTER TABLE `comercio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
---
--- AUTO_INCREMENT de la tabla `fos_user`
---
-ALTER TABLE `fos_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT de la tabla `persona`
---
-ALTER TABLE `persona`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT de la tabla `tipoanuncio`
---
-ALTER TABLE `tipoanuncio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
---
--- AUTO_INCREMENT de la tabla `tipocomercio`
---
-ALTER TABLE `tipocomercio`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `anuncio`
---
-ALTER TABLE `anuncio`
-  ADD CONSTRAINT `FK_4B3BC0D439C278AE` FOREIGN KEY (`tipoanuncio_id`) REFERENCES `tipoanuncio` (`id`),
-  ADD CONSTRAINT `FK_4B3BC0D4F5F88DB9` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`);
-
---
--- Filtros para la tabla `buscar`
---
-ALTER TABLE `buscar`
-  ADD CONSTRAINT `FK_343A50B039C278AE` FOREIGN KEY (`tipoanuncio_id`) REFERENCES `tipocomercio` (`id`);
-
---
--- Filtros para la tabla `comercio`
---
-ALTER TABLE `comercio`
-  ADD CONSTRAINT `FK_419511CDDE4776E9` FOREIGN KEY (`tipocomercio_id`) REFERENCES `tipocomercio` (`id`),
-  ADD CONSTRAINT `FK_419511CDF5F88DB9` FOREIGN KEY (`persona_id`) REFERENCES `persona` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
